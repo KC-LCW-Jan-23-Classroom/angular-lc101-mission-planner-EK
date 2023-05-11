327c5934235be63f445b9aad0557e9c35d0cc182
ErinKace <erinkacerovskis@gmail.com>
2023-05-10 19:33:57 -0500

    a little way into bonus mission

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
+  equipment: object[] = [
+    {name: 'Habitat dome', weightKG: 500},
+    {name:'Drones', weightKG: 40},
+    {name: 'Food containers', weightKG: 10},
+    {name: 'Oxygen tanks', weightKG:60}
+  ];
 
   constructor() { }
 
